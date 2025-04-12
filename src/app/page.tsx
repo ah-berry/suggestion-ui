2c8721bd1175f13c782d91be2ff9be3e56913ea8
Ahmed Gorashi <ahmed@agenthost.ai>
2025-04-12 16:02:32 -0700

    Add basic "dumb" skeleton of the web application

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,97 @@
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
+import { Textarea, Button, Stack } from "@chakra-ui/react";
+import { Spinner } from "@chakra-ui/react";
 
 export default function HomePage() {
+  const [userMessage, setUserMessage] = React.useState<string>("");
+  const [stepType, setStepType] = React.useState<string>("userMessageEntry");
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [playgroundData, setPlaygroundData] = React.useState<string>("");
+
+  const handleSubmit = () => {
+    setIsLoading(true);
+
+    // Mimic waittime response from an async API call as a timeout
+    setTimeout(() => {
+      setPlaygroundData(
+        "I understand your patient's challenge and here are some steps to prevent.",
+      );
+      setStepType("userMessageResponse");
+      setIsLoading(false);
+    }, 2000);
+  };
+
+  const handleReturn = () => {
+    setPlaygroundData("");
+    setStepType("userMessageEntry");
+  };
+
+  const renderSteps = () => {
+    if (stepType === "userMessageEntry") {
+      if (isLoading) {
+        return <Spinner colorPalette="green" size="xl" />;
+      }
+
+      return (
+        <>
+          <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
+            What challenge is your patient facing?
+          </h1>
+          <div>
+            <Textarea
+              placeholder="Ask me anything..."
+              onChange={(e) => setUserMessage(e.target.value)}
+            />
+          </div>
+          <div>
+            <Stack align="center">
+              <Button
+                colorPalette="green"
+                rounded="full"
+                onClick={handleSubmit}
+              >
+                Submit
+              </Button>
+            </Stack>
+          </div>
+        </>
+      );
+    }
+
+    if (stepType === "userMessageResponse") {
+      return (
+        <>
+          <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
+            Suggestion
+          </h1>
+          <div>
+            <p>{playgroundData}</p>
+          </div>
+          <div>
+            <Stack align="center">
+              <Button
+                colorPalette="green"
+                rounded="full"
+                onClick={handleReturn}
+              >
+                New challenge
+              </Button>
+            </Stack>
+          </div>
+        </>
+      );
+    }
+
+    return null;
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[#E9F5F0] text-black">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+        {renderSteps()}
       </div>
     </main>
   );
---
+++ src/app/provider.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+
+export default function Provider(props: { children: React.ReactNode }) {
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        {props.children}
+      </ThemeProvider>
+    </ChakraProvider>
+  );
+}
